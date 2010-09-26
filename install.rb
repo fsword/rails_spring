8e8bf12d1d592bdde2243af1779039cfb5a4c1ea
fsword <li.jianye@gmail.com>
2010-09-26 17:25:47 +0800

    from file is spring_file

--- install.rb
+++ install.rb
@@ -1,12 +1,13 @@
 require 'ftools'
 
 rails_root=File.dirname(__FILE__)+"/../../../"
+spring_file=File.dirname(__FILE__)+'/install_assets/springbeans.xml'
 
 Dir.mkdir rails_root+"config/java" unless File.exist? rails_root+"config/java"
-File.copy('install_assets/springbeans.xml', rails_root+'config/java',true) if rails_root+'config/java/springbeans.xml'
+File.copy(spring_file, rails_root+'config/java',true) if rails_root+'config/java/springbeans.xml'
 
 Dir.mkdir rails_root+"lib/jars" unless File.exist? rails_root+"lib/jars"
 
 Dir.mkdir rails_root+"test/java" unless File.exist? rails_root+"test/java"
-File.copy('install_assets/springbeans.xml', rails_root+'test/java',true) if rails_root+'test/java/springbeans.xml'
+File.copy(spring_file, rails_root+'test/java',true) if rails_root+'test/java/springbeans.xml'
 
