4dd3ef5d2323a41e9b97cfc7cedb43ed1b4db1a1
fsword <li.jianye@gmail.com>
2010-09-26 17:47:18 +0800

    添加启动脚本

--- install.rb
+++ install.rb
@@ -1,13 +1,16 @@
 require 'ftools'
 
 rails_root=File.dirname(__FILE__)+"/../../../"
-spring_file=File.dirname(__FILE__)+'/install_assets/springbeans.xml'
+asset=File.dirname(__FILE__)+'/install_assets/'
 
 Dir.mkdir rails_root+"config/java" unless File.exist? rails_root+"config/java"
-File.copy(spring_file, rails_root+'config/java',true) if rails_root+'config/java/springbeans.xml'
+File.copy(asset+'springbeans.xml', rails_root+'config/java',true) unless File.exist? rails_root+'config/java/springbeans.xml'
 
 Dir.mkdir rails_root+"lib/jars" unless File.exist? rails_root+"lib/jars"
 
 Dir.mkdir rails_root+"test/java" unless File.exist? rails_root+"test/java"
-File.copy(spring_file, rails_root+'test/java',true) if rails_root+'test/java/springbeans.xml'
+File.copy(asset+'springbeans.xml', rails_root+'test/java',true) unless File.exist? rails_root+'test/java/springbeans.xml'
 
+%w{jruby-console jruby-server start.sh}.each{|file|
+  File.copy(asset+file, rails_root+'script',true) unless File.exist? rails_root+'script/'+file
+}
