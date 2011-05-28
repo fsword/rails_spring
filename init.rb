1610c203e57956b8f46d620578f892c0daf255bb
fsword <li.jianye@gmail.com>
2011-05-28 15:58:07 +0800

    Project is not supported yet

--- init.rb
+++
@@ -1,6 +0,0 @@
-begin
-  require 'java'
-  require 'spring_support'
-rescue Exception => e
-  $stderr.puts "cannot load java classes - #{e.to_s}"
-end
--- lib/tasks/spring_support_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :spring_support do
-#   # Task goes here
-# end
